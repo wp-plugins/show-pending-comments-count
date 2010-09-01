1307805ca5d5da0dcf4a9b3029a40baec8c319ef
coffee2code <coffee2code@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-09-01 00:37:33 +0000

    * Update to v1.1 * Don't even define class if not in the admin * Rename class from 'ShowPendingCommentsCount' to 'c2c_ShowPendingCommentsCount' * Output JS via 'admin_print_footer_scripts' hook rather than 'admin_footer' * Assign object instance to global variable, $c2c_show_pending_comments_count, to allow for external manipulation * Instantiate object within primary if(!class_exists()) check (at end) * Note compatibility with WP 3.0+ * Minor code reformatting (spacing) * Remove documentation and instructions from top of plugin file (all of that and more are contained in readme.txt) * Add Upgrade Notice section to readme.txt * Remove trailing whitespace
    git-svn-id: https://plugins.svn.wordpress.org/show-pending-comments-count/tags/1.1@283435 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -3,11 +3,12 @@ Contributors: coffee2code
 Donate link: http://coffee2code.com/donate
 Tags: pending comments, comments, admin, edit posts, coffee2code
 Requires at least: 2.6
-Tested up to: 2.9.1
-Stable tag: 1.0.1
-Version: 1.0.1
+Tested up to: 3.0.1
+Stable tag: 1.1
+Version: 1.1
 
 Display the pending comments count next to the approved comments count in the admin listing of posts.
+
 
 == Description ==
 
@@ -37,6 +38,18 @@ This plugin will only function for users in the admin who have JavaScript enable
 
 == Changelog ==
 
+= 1.1 =
+* Don't even define class if not in the admin
+* Rename class from 'ShowPendingCommentsCount' to 'c2c_ShowPendingCommentsCount'
+* Output JS via 'admin_print_footer_scripts' hook rather than 'admin_footer'
+* Assign object instance to global variable, $c2c_show_pending_comments_count, to allow for external manipulation
+* Instantiate object within primary if(!class_exists()) check (at end)
+* Note compatibility with WP 3.0+
+* Minor code reformatting (spacing)
+* Remove documentation and instructions from top of plugin file (all of that and more are contained in readme.txt)
+* Add Upgrade Notice section to readme.txt
+* Remove trailing whitespace
+
 = 1.0.1 =
 * Add PHPDoc documentation
 * Note compatibility with WP 2.9+
@@ -44,3 +57,9 @@ This plugin will only function for users in the admin who have JavaScript enable
 
 = 1.0 =
 * Initial release
+
+
+== Upgrade Notice ==
+
+= 1.1 =
+Minor update. Miscellaneous tweaks; renamed class; noted compatibility with WP 3.0+.
--- show-pending-comments-count.php
+++ show-pending-comments-count.php
@@ -2,48 +2,30 @@
 /**
  * @package Show_Pending_Comments_Count
  * @author Scott Reilly
- * @version 1.0.1
+ * @version 1.1
  */
 /*
 Plugin Name: Show Pending Comments Count
-Version: 1.0.1
-Plugin URI: http://coffee2code.com/wp-plugins/show-pending-comments-count
+Version: 1.1
+Plugin URI: http://coffee2code.com/wp-plugins/show-pending-comments-count/
 Author: Scott Reilly
 Author URI: http://coffee2code.com
 Description: Display the pending comments count next to the approved comments count in the admin listing of posts.
 
-By default, in the admin listing of posts, each post has its count of approved comments displayed with a word
-bubble background.  If you hover over a comment count, the tooltip hover text indicates the number of pending
-comments.  This plugin utilizes JavaScript to change the post listings so that the pending comments count is 
-displayed next to the approved comments count inside the same word bubble (though with a separator).
+Compatible with WordPress 2.6+, 2.7+, 2.8+, 2.9+, 3.0+.
 
-This plugin will only function for users in the admin who have JavaScript enabled.
-
-The pending comments count will appear next to post comment counts on:
-
-* The "Edit Posts" listing of posts
-* The "Edit Pages" listing of pages
-* The "Edit Comments" listing of comments
-
-Compatible with WordPress 2.6+, 2.7+, 2.8+, 2.9+.
-
-=>> Read the accompanying readme.txt file for more information.  Also, visit the plugin's homepage
-=>> for more information and the latest updates
-
-Installation:
-
-1. Download the file http://coffee2code.com/wp-plugins/show-pending-comments-count.zip and unzip it into your 
-/wp-content/plugins/ directory (or install via the built-in WordPress plugin installer).
-2. Activate the plugin through the 'Plugins' admin menu in WordPress
+=>> Read the accompanying readme.txt file for instructions and documentation.
+=>> Also, visit the plugin's homepage for additional information and updates.
+=>> Or visit: http://wordpress.org/extend/plugins/show-pending-comments-count/
 
 */
 
 /*
 Copyright (c) 2009-2010 by Scott Reilly (aka coffee2code)
 
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation 
-files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, 
-modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the 
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy,
+modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the
 Software is furnished to do so, subject to the following conditions:
 
 The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
@@ -54,20 +36,20 @@ LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRA
 IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
-if ( !class_exists('ShowPendingCommentsCount') ) :
+if ( is_admin() && !class_exists( 'c2c_ShowPendingCommentsCount' ) ) :
 
-class ShowPendingCommentsCount {
+class c2c_ShowPendingCommentsCount {
 	var $comment_column_width = '5em';	// WP default is 4em, which is not sufficient to display 3 digit comments + 2 digit pending
 	var $separator = ' &bull; ';
 
 	/**
 	 * Class constructor: initializes class variables and adds actions and filters.
 	 */
-	function ShowPendingCommentsCount() {
+	function c2c_ShowPendingCommentsCount() {
 		global $pagenow;
-		if ( in_array($pagenow, array('edit.php', 'edit-comments.php', 'edit-pages.php')) ) {
-			add_action('admin_head', array(&$this, 'add_css'));
-			add_action('admin_footer', array(&$this, 'add_js'));
+		if ( in_array( $pagenow, array( 'edit.php', 'edit-comments.php', 'edit-pages.php' ) ) ) {
+			add_action( 'admin_head', array( &$this, 'add_css' ) );
+			add_action( 'admin_print_footer_scripts', array( &$this, 'add_js' ) );
 		}
 	}
 
@@ -102,11 +84,10 @@ CSS;
 
 JS;
 	}
-} // end ShowPendingCommentsCount
+} // end c2c_ShowPendingCommentsCount
+
+$GLOBALS['c2c_show_pending_comments_count'] = new c2c_ShowPendingCommentsCount();
 
 endif; // end if !class_exists()
-
-if ( class_exists('ShowPendingCommentsCount') )
-	new ShowPendingCommentsCount();
 
 ?>
